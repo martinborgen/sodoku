07c5958e474e82bcd8a888744e7da1482c4091bd
Martin Borgén <martinb.nymail@gmail.com>
2024-08-18 00:46:42 +0200

    Implemented a unittest corresponding to previous test code

--- test/test.cpp
+++ test/test.cpp
@@ -31,20 +31,25 @@ void initialize_sodoku() {
     }
 }
 
+void verify_original_vector_is_unchanged() {
+    Sodoku s(9, 3);
+    s = _sodoku_vector;
+
+    s.set_cell(0, 0, 4);
+    TEST_CHECK(_sodoku_vector[0][0] == 0);
+}
+
 void verify_column_access() {
     Sodoku s(9, 3);
     s = _sodoku_vector;
 
-    std::vector<int> col_test;
-    col_test = s.get_col(2);
-
-    for (int i = 0; i < s.get_side_len(); i++) {
-        std::vector<int> col;
-        col.reserve(9);
-        for (int j = 0; j < s.get_side_len(); j++) {
-            col.push_back(s.get_cell(i, j));
+    for (int col = 0; col < s.get_side_len(); col++) {
+        std::vector<int> column;
+        column.reserve(9);
+        for (int row = 0; row < s.get_side_len(); row++) {
+            column.push_back(s.get_cell(row, col));
         }
-        TEST_CHECK(s.get_col(i) == col);
+        TEST_CHECK(s.get_col(col) == column);
     }
 }
 
@@ -55,85 +60,57 @@ void verify_box_access() {
 
 
     for (int count = 0; count < s.get_side_len(); count++) {
-        int i = count % box_len;
-        int j = count / box_len;
+        int i = count / box_len;
+        int j = count % box_len;
         std::vector<int> box;
         box.reserve(s.get_side_len());
         for (int box_i = 0; box_i < box_len; box_i++) {
             for (int box_j = 0; box_j < box_len; box_j++) {
-                box.push_back(_sodoku_vector[i + box_i][j + box_j]);
+                box.push_back(_sodoku_vector[i * box_len + box_i][j * box_len + box_j]);
             }
         }
         TEST_CHECK(box == s.get_nums_in_box(i, j));
     }
 }
 
+void verify_boolean_contains() {
+    Sodoku s(9, 3);
+    s = _sodoku_vector;
+    
+    TEST_CHECK(s.row_contains(0, 8) == true);
+    TEST_CHECK(s.row_contains(2, 5) == false);
+    TEST_CHECK(s.box_contains(1, 1, 9) == true);
+    TEST_CHECK(s.box_contains(1, 1, 7) == false);
+}
+
+void initial_values_check() {
+    Sodoku s(9, 3);
+    s = _sodoku_vector;
+
+    TEST_CHECK(s.is_initial(0, 1) == true);
+    TEST_CHECK(s.is_initial(0, 0) == false);
+}
+
+void verify_solved_count() {
+    Sodoku s(9, 3);
+    s = _sodoku_vector;
+
+    int expected = 31;
+    int produced = s.get_solved_count();
+    TEST_CHECK(produced == expected);
+    TEST_MSG("Expected: %d", expected);
+    TEST_MSG("Produced: %d", produced);
+}
+
+
 TEST_LIST = {
     {"initialize sodoku", initialize_sodoku},
+    {"verify original vector is unchanged", verify_original_vector_is_unchanged},
     {"verify col access", verify_column_access},
     {"verify box access", verify_box_access},
+    {"veryf boolean contains", verify_boolean_contains},
+    {"initial values check", initial_values_check},
+    {"verify solved count", verify_solved_count},
     {NULL, NULL}
 };
 
-    // // Verification of box access
-    // std::cout << "\n";
-    // std::vector<int> box_test;
-    // box_test = s.get_nums_in_box(1, 1);
-    // for (int i = 0; i < s.get_box_side_len() * s.get_box_side_len(); i++) {
-    //     std::cout << box_test[i];
-    // }
-    // std::cout << "\n";
-
-    // // verification of boolean contains function
-    // if (s.row_contains(0, 8)) {
-    //     std::cout << "row 0 contains 8\n";
-    // } else {
-    //     std::cout << "Error row_contains\n";
-    // }
-    // if (!s.row_contains(2, 5)) {
-    //     std::cout << "row 2 does not contain 5\n";
-    // } else {
-    //     std::cout << "Error col_contains\n";
-    // }
-    // if (s.box_contains(1, 1, 9)) {
-    //     std::cout << "Box (1, 1) contains 9\n";
-    // } else {
-    //     std::cout << "Error box contains\n";
-    // }
-    // if (!s.box_contains(1, 1, 7)) {
-    //     std::cout << "Box (1, 1) does not contain 7\n";
-    // } else {
-    //     std::cout << "ERROR box contains\n";
-    // }
-
-    // // Initial values check
-    // if (s.is_initial(0, 1)) {
-    //     std::cout << "Square 0, 1 is initial\n";
-    // } else {
-    //     std::cout << "ERROR in is_initial!\n";
-    // }
-
-    // if (!s.is_initial(0, 0)) {
-    //     std::cout << "Square 0, 0 is not initial\n";
-    // } else {
-    //     std::cout << "ERROR in is_initial!\n";
-    // }
-
-    // // Verification of solved count
-    // if (s.get_solved_count() == 31) {
-    //     std::cout << "Solved is 31\n";
-    // } else {
-    //     std::cout << "Error in solved count";
-    // }
-
-
-    // std::cout << "\n verificiation original is unchanged:\n";
-
-    // for (int i = 0; i < sodoku.size(); i++) {
-    //     for (int j = 0; j < sodoku[0].size(); j++) {
-    //         std::cout << sodoku[i][j];
-    //     }
-    //     std::cout << "\n";
-    // }
-
-    // return 0;
